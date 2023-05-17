22e544d24aff852fcd8b4c5586b4d2370f4151f2
Joel Santos <joeltere9@gmail.com>
2023-05-17 18:40:26 -0300

    created method post put and delete

--- src/main/java/com/employee/employer/controller/EmployeeController.java
+++ src/main/java/com/employee/employer/controller/EmployeeController.java
@@ -1,16 +1,55 @@
 package com.employee.employer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.employer.model.Employee;
 import com.employee.employer.service.EmployeeService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService es;
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getEmployee() {
+
+        return ResponseEntity.ok().body(es.getAllEmployee());
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+
+        return es.create(employee);
+
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+        Employee employee1 = es.update(id, employee);
+        return ResponseEntity.ok().body(employee1);
+
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        es.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/employee/employer/service/EmployeeService.java
+++ src/main/java/com/employee/employer/service/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.employee.employer.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.employee.employer.model.Employee;
 import com.employee.employer.repository.EmployeeRepostory;
 
 @Service
@@ -10,4 +14,40 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepostory employeeRepostory;
+
+    // Find employee by id
+    public Employee findById(Long id) {
+        Optional<Employee> employee = employeeRepostory.findById(id);
+
+        return employee.orElseThrow();
+    }
+
+    // get All the employee
+
+    public List<Employee> getAllEmployee() {
+        List<Employee> employee = employeeRepostory.findAll();
+
+        return employee;
+    }
+
+    public Employee create(Employee employee) {
+
+        return employeeRepostory.save(employee);
+    }
+
+    public Employee update(long id, Employee employee) {
+        Employee employee2 = findById(id);
+
+        employee2.setName(employee.getName());
+
+        employee2.setLastName(employee.getLastName());
+
+        employee2.setEmail(employee.getEmail());
+
+        return employeeRepostory.save(employee2);
+    }
+
+    public void delete(long id) {
+        employeeRepostory.deleteById(id);
+    }
 }
